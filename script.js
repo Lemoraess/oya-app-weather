e363b9f49a74b3d15920aa52c0a80e165065f402
Lenise Moraes <lemoraes.santana@gmail.com>
2025-03-11 18:44:10 -0300

    Added more API integration data

--- script.js
+++ script.js
@@ -2,10 +2,42 @@ function currentData(response) {
   let temperatureElement = document.querySelector("#degreesData");
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+
   let cityElement = document.querySelector("#cityData");
   cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#descriptionData");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidityData");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windElement = document.querySelector("#windData");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#timeData");
+  timeElement.innerHTML = showDay(date);
 }
 
+function showDay(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function apiCall(city) {
   let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
