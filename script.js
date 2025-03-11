05e6225453cea015a435c280c672812da9079099
Lenise Moraes <lemoraes.santana@gmail.com>
2025-03-11 17:59:06 -0300

    Added API integration.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function showWeather(event) {
+function currentData(response) {
+  let temperatureElement = document.querySelector("#degreesData");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#cityData");
+  cityElement.innerHTML = response.data.city;
+}
+
+function apiCall(city) {
+  let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentData);
+}
+
+function showCity(event) {
   event.preventDefault();
   let searchBox = document.querySelector("#searchBox");
-  let city = document.querySelector("#cityData");
-  city.innerHTML = searchBox.value;
+  apiCall(searchBox.value);
 }
 
 let searchForm = document.querySelector("#searchForm");
-searchForm.addEventListener("submit", showWeather);
+searchForm.addEventListener("submit", showCity);
+
+apiCall("Ubatuba");
